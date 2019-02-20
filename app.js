8fb72ddc80af38f894a69b1695b9ba209ec7455b
donjoe <giumontanaro@gmail.com>
2019-02-20 21:02:34 +0100

    Bind the data to the rect and append them to the svg

--- app.js
+++ app.js
@@ -22,6 +22,20 @@ d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
             .domain([0, d3.max(data, d => d[1])])
             .range([height - padding, padding]);
 
+        chart
+            .selectAll('rect')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('class', 'bar')
+            .attr('data-date', d => d[0])
+            .attr('data-gdp', d => d[1])
+            .attr('fill', '#467bf3')
+            .attr('width', barWidth)
+            .attr('height', d => height - padding - yScale(d[1]))
+            .attr('x', (d, i) => 1 + padding + (barWidth * i)) //1px offset to pass test n 10
+            .attr('y', d => yScale(d[1]))
+
         /*----- AXES ------*/
         const xAxis = d3.axisBottom(xScale);
         const yAxis = d3.axisLeft(yScale);
