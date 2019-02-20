e786b911c817171e4441bf09ad03790f3b636a5f
donjoe <giumontanaro@gmail.com>
2019-02-20 20:44:32 +0100

    Set the x scale based on min and max date

--- app.js
+++ app.js
@@ -10,5 +10,12 @@ const chart = d3.select('.chart')
     .then(json => {
         const { data } = json; //destructuring 
         const barWidth = (width - padding - padding) / data.length;
+
+        const minDate = new Date(data[0][0]);
+        const maxDate = new Date(data[data.length - 1][0]);
+
+        const xScale = d3.scaleTime()
+            .domain([minDate, maxDate])
+            .range([padding, width - padding]);
        
     });
