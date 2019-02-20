7bf37a450178ec64511abee6c0dd9042f1dc97c0
donjoe <giumontanaro@gmail.com>
2019-02-20 20:39:24 +0100

    Fetch the data from url

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const padding = 50;
 const chart = d3.select('.chart')
     .attr('width', width)
     .attr('height', height);
+
+    d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+    .then(json => {
+        const { data } = json; //destructuring        
+    });
